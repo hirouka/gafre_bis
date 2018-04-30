03af83d7f285421e0dcd8294783df0b9f944ca00
madeforfame <drobecqa@F213-09.e-im2ag.ujf-grenoble.fr>
2018-04-30 15:31:54 +0200

    Test manger

--- src/test/java/GaufreTest.java
+++ src/test/java/GaufreTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.*;
+
 import waffle.*;
+import general.*;
+import player.*;
+import player.*;
 
 import org.junit.Test;
 
@@ -61,5 +65,75 @@ public class GaufreTest {
 		Iterator i= g.getIterator();
 		assertTrue("Iterator est NULL", i !=null);
 		
-	}		
+	}	
+        
+        @Test
+        public void testMangerUneCase(){
+		Gaufre g = new Gaufre(5,5);
+                Case c = new Case(4,4);
+                g.manger(c);
+                assertEquals("La case n'est pas mangée", CaseType.EATEN, g.getCase(4,4));
+	}
+        
+        @Test
+        public void testMangerPlusieursCases(){
+		Gaufre g = new Gaufre(5,5);
+                Case c = new Case(3,3);
+                int k=0;
+                g.manger(c);
+                if(g.getCase(3,3)!=CaseType.EATEN){
+                    k++;
+                }
+                if(g.getCase(4,3)!=CaseType.EATEN){
+                    k++;
+                }
+                if(g.getCase(3,4)!=CaseType.EATEN){
+                    k++;
+                }
+                if(g.getCase(4,4)!=CaseType.EATEN){
+                    k++;
+                }
+                assertEquals("On ne mange pas toutes les cases", k, 0);
+	}
+        
+        @Test
+        public void testMangerVides(){
+		Gaufre g = new Gaufre(5,5);
+                Case c = new Case(3,3);
+                g.setCase(3,3,CaseType.EATEN);
+                int k=0;
+                g.manger(c);
+                if(g.getCase(3,3)!=CaseType.EATEN){
+                    k++;
+                }
+                if(g.getCase(4,3)!=CaseType.EATEN){
+                    k++;
+                }
+                if(g.getCase(3,4)!=CaseType.EATEN){
+                    k++;
+                }
+                if(g.getCase(4,4)!=CaseType.EATEN){
+                    k++;
+                }
+                assertEquals("Manger une case vide fait n'importe quoi", k, 0);
+	}
+        @Test
+        public void testMangerPoison(){
+		Gaufre g = new Gaufre(5,5);
+                Case c = new Case(0,0);
+                int k=0, i, j;
+                g.manger(c);
+                
+                for(i=0;i<5;i++){
+                    for(j=0;j<5;j++){
+                        if(g.getCase(i,j)!=CaseType.EATEN){
+                            k++;
+                        }
+                    }
+                }
+                
+                assertEquals("Manger le poison ne marche pas", k, 0);
+                
+	}
+        
 }
